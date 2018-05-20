f3b8639ed45fa5e9411a9083c26dbf779edbb586
christian1006 <37366207+christian1006@users.noreply.github.com>
2018-05-20 18:12:25 +0200

    Update animals.js

--- js/animals.js
+++ js/animals.js
@@ -26,7 +26,7 @@ var vue = new Vue({
         getAnimalById: function (id) {
             axios.get('http://localhost:3000/animals')
             .then(function (response) {
-                /* code for adding data from response to the animal array */
+                (response.data.animals)/* code for adding data from response to the animal array */
             })
             .catch(function (error) {
                 console.log(error);
