fdcbabc5149b78411ab7b5a1d14734ebd3157199
H3xas <czabajrafal@gmail.com>
2018-05-20 13:48:35 +0200

    Example of watches, methods and computed properties

--- js/main.js
+++ js/main.js
@@ -7,31 +7,61 @@ var vue = new Vue({
         isEditable: false,
         titleContainer: {
             h1title: 'h1 title',
-        }
+        },
+        question: '',
+        answer: 'I cannot give you an answer until you ask a question!'
     },
 
     computed: {
-        
-        fullname: function() {
+
+        fullname: function () {
             return 'Hi ' + this.name + ' ' + this.lastname;
         },
-        randomNumber: function() {
+        randomNumber: function () {
             return Math.random();
         },
-        onetimeComputedIsValid: function() {
+        onetimeComputedIsValid: function () {
             let isValid = validator.isValid(this.myNumber);
             console.log('myNumber is', isValid ? 'greater' : 'smaller', 'than 0.5');
             return isValid;
         },
 
-        onceCurrentDate: function() {
+        onceCurrentDate: function () {
             return Date.now();
         }
     },
 
     methods: {
-        currentDate: function() {
+        currentDate: function () {
             return Date.now();
-        }
+        },
+
+        // getAnswer: function () {
+        //     if (this.question.indexOf('?') === -1) {
+        //         this.answer = 'Questions usually contain a question mark. ;-)'
+        //         return
+        //     }
+        //     this.answer = 'Thinking...'
+        //     var vm = this
+        //     axios.get('https://yesno.wtf/api')
+        //         .then(function (response) {
+        //             vm.answer = _.capitalize(response.data.answer)
+        //         })
+        //         .catch(function (error) {
+        //             vm.answer = 'Error! Could not reach the API. ' + error
+        //         })
+        // }
     },
+
+    // watch: {
+    //     question: function (newQuestion, oldQuestion) {
+    //         this.answer = 'Waiting for you to stop typing...'
+    //         this.debouncedGetAnswer()
+    //     }
+    // },
+
+    // created: function () {
+    //     this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    // },
+
 });
