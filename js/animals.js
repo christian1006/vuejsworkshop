093d3ca453de6935c5019bc45c19f09aa886d7b8
H3xas <czabajrafal@gmail.com>
2018-05-20 17:47:14 +0200

    adding animals

---
+++ js/animals.js
@@ -0,0 +1,36 @@
+var vue = new Vue({
+    el: '#app',
+    data: {
+        animalId: undefined,
+        singleAnimal: [],
+        animals: []
+    },
+
+    computed: {
+        simpleAnimal: function () {
+            return this.singleAnimal.length != 0 ? this.singleAnimal[0] : { id: -1 };
+        }
+    },
+
+    methods: {
+        getAnimals: function () {
+            axios.get('http://localhost:3000/animals')
+                .then(function (response) {
+                    response.data.forEach(element => vue.animals.push(element));
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+
+        getAnimalById: function (id) {
+            axios.get(/* Put here the url */)
+            .then(function (response) {
+                /* code for adding data from response to the animal array */
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+    }
+});
