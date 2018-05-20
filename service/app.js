101986589bec8980c6c06a1f24fbb7a563690e32
H3xas <czabajrafal@gmail.com>
2018-05-20 17:21:51 +0200

    Adding animallist excercise solution. Adding get animal by id method

--- service/app.js
+++ service/app.js
@@ -1,9 +1,21 @@
 let express = require('express');
 let storage = require('./storage');
 let app = express();
+let cors = require('cors');
+app.use(cors());
 
 app.get('/animals', (req, res) => {
     res.send(storage.data);
+});
+
+app.get('/animals/:id', (req, res) => {
+    let requestId = req.params.id;
+    let animal = storage.data.find((animal) => animal.id == requestId);
+    if (animal != undefined) {
+        res.send(animal);
+    } else {
+        res.send({ error: 'animal does not exist' });
+    }
 });
 
 app.listen(3000, () => {
